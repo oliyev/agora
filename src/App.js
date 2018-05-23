37a95b9c14a55c15194a6fdccd61bb76ed9839de
Unknown <olivierlepage@protonmail.com>
2018-05-22 23:16:25 -0400

    setup somewhat completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import '../style/App.css';
+import './style/main.css';
+import './style/App.css';
 
 class App extends Component {
   render() {
@@ -11,8 +12,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, dsafedit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="homepage">
+          <span>hello</span>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../style/index.css';
 import App from './App';
+import Homepage from './views/Homepage';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
