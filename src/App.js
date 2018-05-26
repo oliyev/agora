bf5cdc73c847039aef7142e2f675c999c3e8a458
Unknown <olivierlepage@protonmail.com>
2018-05-26 15:10:06 -0400

    Auto stash before merge of "dev" and "origin/dev"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './style/App.css';
-import './style/main.css';
-import './vendor/bootstrap/css/bootstrap.min.css';
-import './vendor/font-awesome/css/font-awesome.min.css';
-import './vendor/magnific-popup/magnific-popup.css';
-import './css/creative.min.css';
-import './vendor/bootstrap/css/bootstrap.min.css';
 
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
-import App from './App';
 import Homepage from './views/Homepage';
 import Chatroom from './views/Chatroom';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/creative.min.css';
 
 ReactDOM.render(<Chatroom />, document.getElementById('root'));
 registerServiceWorker();
