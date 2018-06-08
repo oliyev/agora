747138f599a90cc8cb9e368bebd423e3faa6b9f8
Unknown <olivierlepage@protonmail.com>
2018-06-07 21:53:31 -0400

    TWEAKS

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cluster = require('cluster');
 const child_process = require('child_process')
 const Debate = require('./Debate');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 let q = 'r-409089';
 let debates = [];
@@ -81,7 +81,7 @@ io.on('connection', (socket) => {
 
 })
 
-server.listen(PORT, () => console.log('Listening on port 4000!'));
+server.listen(PORT, () => console.log('Listening on port ' + PORT));
 
 ///////////////////// DEBATE METHODS
 debateTimerHandler = (debate) => {
--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const HTTP = axios.create({
   // baseURL: 'http://localhost:4000/',
-  baseURL: 'https://agoraproject.herokuapp.com/',
+  baseURL: process.env.baseURL || "http://localhost:3000",
   headers: {}
 });
 
